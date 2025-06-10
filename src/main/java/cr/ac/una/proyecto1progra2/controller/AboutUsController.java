af13b81989fee9f75941709a0f912aec08afd3b5
Cristhian Cordero Varela <cristhian.cordero.varela@est.una.ac.cr>
2025-06-10 12:14:57 -0600

    bugs

--- src/main/java/cr/ac/una/proyecto1progra2/controller/AboutUsController.java
+++ src/main/java/cr/ac/una/proyecto1progra2/controller/AboutUsController.java
@@ -1,6 +1,5 @@
 package cr.ac.una.proyecto1progra2.controller;
 
-import cr.ac.una.proyecto1progra2.util.FlowController;
 import cr.ac.una.proyecto1progra2.util.MusicManager;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,14 +8,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class AboutUsController extends Controller implements Initializable {
 
    @FXML
 private void onActionBtnVolver(ActionEvent event) {
+    MusicManager.stopEffect(); 
     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     stage.close();
 }
+
+@Override
+public void setStage(Stage stage) {
+    super.setStage(stage);                       
+    if (stage != null) {
+        stage.addEventHandler(WindowEvent.WINDOW_HIDDEN,
+            e -> MusicManager.stopEffect());     
+    }
+}
+
 
 
   @FXML private void onActionBtnJefferson() {
@@ -32,6 +43,9 @@ private void onActionBtnVolver(ActionEvent event) {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        
+        
     }
 
     @Override
